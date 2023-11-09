38d2e086dd0b8981b77d94f8673a05c3d0449596
jepeloa <javier.epeloa@mapplics.com>
2023-11-09 14:18:00 -0300

    porcentual de matching

--- sorter_beta.py
+++ sorter_beta.py
@@ -321,7 +321,7 @@ def main():
         for i in skills:
             s += "- " + i + "\n"
         st.markdown(s)
-        df_sorted_from_db['MatchValue']=1-(df_sorted_from_db['MatchValue']/df_sorted_from_db['MatchValue'].max())
+        df_sorted_from_db['MatchValue']=((1-(df_sorted_from_db['MatchValue']/df_sorted_from_db['MatchValue'].max()))/(1-(df_sorted_from_db['MatchValue']/df_sorted_from_db['MatchValue'].max())).max())*100
         fig = px.bar(df_sorted_from_db, x='Filename', y='MatchValue', title='Match Values by Filename')
 
         # Mostrar el gráfico en Streamlit
