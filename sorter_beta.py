618c14015be94bbee858d2ca4df5e2fdb6fa9767
jepeloa <javier.epeloa@mapplics.com>
2023-11-09 11:50:09 -0300

    fdf

--- sorter_beta.py
+++ sorter_beta.py
@@ -139,11 +139,13 @@ def delete_files_in_directory(directory):
 
 
 if uploaded_files:
-        if st.sidebar.button('Guardar PDFs'):
-            save_uploaded_files(uploaded_files)
+       # if st.sidebar.button('Guardar PDFs'):
+        #    save_uploaded_files(uploaded_files)
+        pass
 if JD_files:
-    if st.button('Upload JD_file'):
-        save_JD_files(JD_files)
+   # if st.button('Upload JD_file'):
+     #   save_JD_files(JD_files)
+     pass
 
 
 
