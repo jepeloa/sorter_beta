aa89b911413412cdebadc4fab8fd14fedfb5378e
jepeloa <javier.epeloa@mapplics.com>
2023-11-10 17:03:51 -0300

    procesar boton

--- sorter_beta.py
+++ sorter_beta.py
@@ -287,8 +287,9 @@ def main():
 
     
     if uploaded_files:
-        file_data = read_CV_from_pdf(path_to_folder)  #extraigo datos de los pdf
-        cv_collection=store_CV_in_db(file_data)
+        if st.sidebar.button("procesar"):
+            file_data = read_CV_from_pdf(path_to_folder)  #extraigo datos de los pdf
+            cv_collection=store_CV_in_db(file_data)
         
             
     if delete_query:
