db3e7703ee658535ac09f51b300825e956278ca7
jepeloa <javier.epeloa@mapplics.com>
2023-11-14 16:51:44 -0300

    eliminar nombre de file

--- sorter_beta.py
+++ sorter_beta.py
@@ -326,7 +326,6 @@ def main():
             user_prompt=f"quiero que extraigas el primer nombre de archivo con extension pdf del siguiente texto: {cv_selected}. Solo indica el nombre de archivo en tu respuesta. Por ejemplo javier.pdf"
             File=chat_gpt_action(system_prompt,user_prompt)
             st.write(cv_selected['content'])
-            st.write(File['content'])
             store_to_sqlite(df_sorted)
         else:
             st.write("Please enter a job description to process.")
