190ef435b06017535bea4a10ed8a2e8281936326
jepeloa <javier.epeloa@mapplics.com>
2023-11-14 16:15:48 -0300

    borrada libreria openai

--- sorter_beta.py
+++ sorter_beta.py
@@ -24,9 +24,8 @@ import time
 from pyresparser import ResumeParser
 import openai
 openai.api_key = os.environ["OPENAI_API_KEY"]
-from openai import OpenAI
 
-client = OpenAI()
+
 
 jd=' '
 
@@ -61,60 +60,6 @@ def chat_gpt_action(system,prompt):
     return message
 
  
-def chat_gpt_assistant(system,prompt):
-
-    client = OpenAI()
-
-    # file = client.files.create(
-    #     file=open("files/output_san_cristobal/DS_1.pdf", "rb"),
-    #     purpose="assistants"
-    # )
-
-    # print("FILE ID: ", file.id)
-
-
-    # assistant = client.beta.assistants.create(
-    #     name="Data visualizer",
-    #     model="gpt-3.5-turbo-1106",
-    #     tools=[{"type": "retrieval"}],
-    #     file_ids=[file.id]
-    # )
-
-    # thread = client.beta.threads.create()
-
-    # print("THREAD ID: ", thread.id)
-
-    message = client.beta.threads.messages.create(
-        thread_id="thread_objI64Kj60FdS2EQrNVB04EN",
-        role="user",
-        content=f"{prompt}",
-        file_ids=["file-5rQuwUoQzjjKSX8XvTc8W9eJ"]
-    )
-
-    print("MESSAGE ID: ", message.id)
-
-    run = client.beta.threads.runs.create(
-        thread_id="thread_objI64Kj60FdS2EQrNVB04EN",
-        assistant_id="asst_pSbwUNXpra1hWUQ8cKN8in6U",
-        model="gpt-4-1106-preview",
-        tools=[{"type": "retrieval"}]
-    )
-
-    print("RUN ID: ", run.id)
-
-    while True:
-        run = client.beta.threads.runs.retrieve(thread_id="thread_objI64Kj60FdS2EQrNVB04EN", run_id=run.id)
-        if (run.completed_at):
-            print("ESPERANDO...")
-            break
-
-    messages = client.beta.threads.messages.list(thread_id="thread_objI64Kj60FdS2EQrNVB04EN")
-    last_message = messages.data[0]
-    text = last_message.content[0].text.value
-    print("RESPUESTA: ", text)
-    return text
-
-
 
 image = Image.open('logo.png')
 
