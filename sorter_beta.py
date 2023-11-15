bb6c856b9910c3cfa4755d801b8bd90a5a035333
jepeloa <javier.epeloa@mapplics.com>
2023-11-15 12:40:55 -0300

    dv

--- sorter_beta.py
+++ sorter_beta.py
@@ -350,9 +350,10 @@ def main():
             selected_pdf = st.selectbox('Elige un PDF:', df_sorted_from_db['Filename'].tolist())
         pdf_url = f"http://143.198.139.51:8081/CV/{selected_pdf}"
         skills=obtain_skills(f"./CV/{selected_pdf}")
-        if jd:
-            st.text("Resultado")
-            st.markdown(str(cv_selected['content']))
+        if st.button('procesar query'):
+            if jd:
+                st.text("Resultado")
+                st.markdown(str(cv_selected['content']))
         st.markdown(f'<iframe src="{pdf_url}" width="700" height="900"></iframe>', unsafe_allow_html=True)
         s=""
         for i in skills:
