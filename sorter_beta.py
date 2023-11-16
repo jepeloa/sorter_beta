77f3ef58f9edf279f23ed93aa4e305eca6621061
jepeloa <javier.epeloa@mapplics.com>
2023-11-16 08:51:54 -0300

    15 cv

--- sorter_beta.py
+++ sorter_beta.py
@@ -312,7 +312,7 @@ def main():
             #jdsum=chat_gpt_action(jd,system_prompt,user_prompt)
             #print("{}: {}".format(jdsum['role'], jdsum['content']))
             #delete_table_contents()
-            results=read_chroma_db(jd,20)
+            results=read_chroma_db(jd,15)
             file_values = [meta['source'] for meta in results['metadatas'][0]]
             match_values = results['distances'][0]
             documents=results['documents'][0]
